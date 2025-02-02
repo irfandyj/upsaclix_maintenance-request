5d9381c57e371e2be03b91ab152c064963020420
Jip Irfandy <irfandyjip98@gmail.com>
2025-02-03 02:36:35 +0700

    feat(web): add unfinished create page

---
+++ packages/web/src/components/organisms/CreateMaintenanceTicketForm/CreateMaintenanceTicketForm.tsx
@@ -0,0 +1,28 @@
+const CreateMaintenanceTicketForm = () => {
+  return (
+    <div>
+      CreateMaintenanceTicketForm
+      {/* <Formik
+        initialValues={{
+          description: '',
+          priority: '',
+          status: '',
+          title: '',
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        <Form>
+          <Field name="title" />
+          <Field name="description" />
+          <Field name="priority" />
+          <Field name="status" />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik> */}
+    </div>
+  );
+}
+
+export default CreateMaintenanceTicketForm;
---
+++ packages/web/src/components/pages/CreateMaintenanceTicketPage/CreateMaintenanceTicketPage.tsx
@@ -0,0 +1,27 @@
+import CreateMaintenanceTicketForm from '@/components/organisms/CreateMaintenanceTicketForm';
+import Link from 'next/link';
+import React from 'react';
+
+const MaintenanceRequestDashboard = () => {
+  const stats = [
+    { title: 'Open Requests', value: 2 },
+    { title: 'Urgent Requests', value: 3 },
+    { title: 'Average time (days) to respond', value: 3 },
+  ]
+
+  return (
+    <div className="CreateMaintenanceTicketPage mx-auto flex flex-col p-6 max-w-4xl ">
+      <div className='flex mt-[68px] mb-[22px] items-center gap-[33px] mx-auto'>
+        {/* Back Arrow Icons */}
+        <Link href="/">Back</Link>
+        <h1 className="text-xl font-semibold ">Maintenance Request</h1>
+      </div>
+      
+      {/* Create Maintenance Ticket Form */}
+      <CreateMaintenanceTicketForm />
+
+    </div>
+  );
+};
+
+export default MaintenanceRequestDashboard;

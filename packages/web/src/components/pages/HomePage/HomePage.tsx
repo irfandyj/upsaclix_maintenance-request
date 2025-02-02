1ced6d325d1850b56f2e5ef611541ee747b6085b
Jip Irfandy <irfandyjip98@gmail.com>
2025-02-02 20:09:22 +0700

    feat(web): remove unused files, use new font

---
+++ packages/web/src/components/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MaintenanceRequestDashboard = () => {
+
+  return (
+    <div className="p-6 max-w-4xl mx-auto">
+      <h1 className="text-2xl font-semibold">Maintenance Request</h1>
+    </div>
+  );
+};
+
+export default MaintenanceRequestDashboard;

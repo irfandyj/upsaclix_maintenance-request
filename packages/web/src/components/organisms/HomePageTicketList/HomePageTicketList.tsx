7748203a24fe77d4b99158fa48d59a730c2bde91
Jip Irfandy <irfandyjip98@gmail.com>
2025-02-03 15:12:05 +0700

    refactor(organisms): make it easy to digest

--- packages/web/src/components/organisms/HomePageTicketList/HomePageTicketList.tsx
+++ packages/web/src/components/organisms/HomePageTicketList/HomePageTicketList.tsx
@@ -2,16 +2,19 @@
 import { useQuery } from "@tanstack/react-query";
 import { getMaintenanceTickets } from "@/lib/graphql/api";
 import TicketListCard from "@/components/molecules/TicketListCard";
-import { useEffect } from "react";
+import { useMemo } from "react";
 import { MaintenanceTicket } from "@/lib/graphql/sdk";
 
 const HomePageTicketList = () => {
-  const tickets: MaintenanceTicket[] = []
-
   const { data } = useQuery({
     queryKey: ['maintenance-tickets'],
     queryFn: () => getMaintenanceTickets(),
   })
+
+  const tickets: MaintenanceTicket[] = useMemo(() => {
+    return data?.getTickets ?? []
+  }, [data])
+
 
   return (
     <ul className="HomePageTicketList pt-5 px-[30.5px] flex flex-col gap-5">
